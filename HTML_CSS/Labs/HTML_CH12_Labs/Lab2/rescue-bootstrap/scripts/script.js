30e9c79d645361cc0bc56adc5e5f8753e87bd9d4
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-09-30 08:49:51 -0500

    ch12 lab2 finish

--- HTML_CSS/Labs/HTML_CH12_Labs/Lab2/rescue-bootstrap/scripts/script.js
+++ HTML_CSS/Labs/HTML_CH12_Labs/Lab2/rescue-bootstrap/scripts/script.js
@@ -3,3 +3,8 @@
     File Name: script.js
     Date: 
 */
+
+//jQuery for hero image to consume the header window space
+    $(document).ready(function(){
+    $('.hero').height($(window).height());
+});
