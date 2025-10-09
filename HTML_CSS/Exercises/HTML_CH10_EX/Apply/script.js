7ca9fd14f18cffc53faee0292bf5083d9d133edd
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-10-09 11:57:11 -0500

    apply ch 10 finish

---
+++ HTML_CSS/Exercises/HTML_CH10_EX/Apply/script.js
@@ -0,0 +1,8 @@
+/*Daniel, 10,9,25*/
+
+function content() {
+  var text = document.getElementById("new");
+  text.textContent = "Hi. I am a new paragraph that was created with a JavaScript function";
+  text.style.color = "#c0145f";
+  text.style.fontsize = "2em";
+}
