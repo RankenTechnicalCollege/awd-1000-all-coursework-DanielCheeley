02537c5f1fdda21a77fab4e9d3aa201ea848c1b7
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-10-10 11:51:33 -0500

    ch10 lab2 step15

--- HTML_CSS/Labs/HTML_CH10_Labs/Lab2/scripts/script.js
+++ HTML_CSS/Labs/HTML_CH10_Labs/Lab2/scripts/script.js
@@ -1,9 +1,13 @@
 function hamburger() {
   var menu = document.querySelector("#menu-links");
+  var menuicon = document.querySelector("#menu-icon");
+  var hamburger = document.querySelector("#hamburger")
   if (menu.style.display === "block") {
     menu.style.display = "none";
+    menuicon.style.color = "#2a1f14";
   }
   else {
     menu.style.display = "block";
+    menuicon.style.color = "#f6eee4";
   }
 }
