2bc746a53d4894e1960d54f36a8415a753e488c1
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-10-10 09:03:28 -0500

    ch10 extend finish

--- HTML_CSS/Exercises/HTML_CH10_EX/Extend/scripts/script.js
+++ HTML_CSS/Exercises/HTML_CH10_EX/Extend/scripts/script.js
@@ -1,5 +1,13 @@
 /*
-    Student Name:
+    Student Name: Daniel Cheeley
     File Name: script.js
-    Date: 
+    Date: 10,10,25
 */
+$(function(){
+  $("#hide").click(function(){
+    $("#main p").hide();
+  });
+  $("#show").click(function(){
+    $("#main p").show();
+  });
+});
