88e1b22dc18a3c446f77617f658b44186ea3c000
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-10-10 09:30:12 -0500

    analyze ch10 finish

--- HTML_CSS/Exercises/HTML_CH10_EX/Analyze/scripts/script.js
+++ HTML_CSS/Exercises/HTML_CH10_EX/Analyze/scripts/script.js
@@ -1,12 +1,32 @@
 /*
-    Student Name:
+    Student Name: Daniel Cheeley
     File Name: script.js
-    Date: 
+    Date: 10/10/25
 */
 
+var figElement = document.getElementById("placeholder")
+var imgSource = document.getElementById("image")
+var figCap = document.getElementById("figcaption")
 //Function to display the first picture
-function pic1 {
+function pic1() {
     imgSource.src = "images/trunk-bay.jpg"
-    imgSource.alt = Elevated view of Trunk Bay beach on St. John";
-    figElement.style.display = "block
-    figCap.textContent = "Trunk Bay in St. John";
+    imgSource.alt = "Elevated view of Trunk Bay beach on St. John";
+    figElement.style.display = "block";
+    figCap.textContent = "Trunk Bay in St. John"; 
+}
+
+//Fucntion to display second picture
+function pic2() {
+    imgSource.src = "images/sanjuan.jpg"
+    imgSource.alt = "Elevated view of San Juan coast";
+    figElement.style.display = "block";
+    figCap.textContent = "Coast of San Juan"; 
+}
+
+//function to display the third picture
+function pic3() {
+    imgSource.src = "images/curacao.jpg";
+    imgSource.alt = "The blue waters of Curacao";
+    figElement.style.display = "block";
+    figCap.textContent = "Curacao";
+}
