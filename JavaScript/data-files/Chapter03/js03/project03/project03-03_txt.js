04b015ff029427793da7feaefb3e75de114b00e1
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-10-28 09:21:13 -0500

    project3 finish

--- JavaScript/data-files/Chapter03/js03/project03/project03-03_txt.js
+++ JavaScript/data-files/Chapter03/js03/project03/project03-03_txt.js
@@ -3,8 +3,8 @@
       Project 03-03
 
       Application to generate a movie list
-      Author: 
-      Date:   
+      Author: Daniel Cheeley
+      Date:   10/28/25
 
       Filename: project03-03.js
 */
@@ -58,3 +58,17 @@ links[8] = "https://www.imdb.com/title/tt1375666/?ref_=adv_li_tt";
 links[9] = "https://www.imdb.com/title/tt0137523/?ref_=adv_li_tt";
 
 
+let htmlCode = "";
+
+for (let i = 0; i < titles.length; i++) {
+      htmlCode += `
+    <tr>
+      <td><a href="${links[i]}">${titles[i]}</a></td>
+      <td>${summaries[i]}</td>
+      <td>${ratings[i]}</td>
+    </tr>`;
+}
+
+let tableBody = document.getElementsByTagName("tbody")[0];
+
+tableBody.innerHTML = htmlCode;
