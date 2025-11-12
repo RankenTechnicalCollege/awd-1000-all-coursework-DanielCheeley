d4656288b6de5501d79b849c2cb2e2e127bf6762
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-11-12 11:43:39 -0600

    ch5 project 2 done

--- JavaScript/data-files/Chapter05/js05/project02/project05-02_txt.js
+++ JavaScript/data-files/Chapter05/js05/project02/project05-02_txt.js
@@ -4,9 +4,26 @@
       Project 05-02
 
       Project to move images between a photo bucket and photo list.
-      Author: 
-      Date:   
+      Author: Daniel Cheeley
+      Date:   11/12/25
 
       Filename: project05-02.js
 */
 
+let images = document.getElementsByTagName("img");
+let photoBucket = document.getElementById("photo_bucket");
+let photoList = document.getElementById("photo_list");
+
+for (let i = 0; i < images.length; i++) {
+  images[i].onclick = function () {
+    if (this.parentElement.id === "photo_bucket") {
+      let newItem = document.createElement("li");
+      photoList.appendChild(newItem);
+      newItem.appendChild(this);
+    } else {
+      let oldItem = this.parentElement;
+      photoBucket.appendChild(this);
+      oldItem.parentElement.removeChild(oldItem);
+    }
+  };
+}
