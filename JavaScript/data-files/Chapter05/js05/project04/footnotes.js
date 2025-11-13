4b39aa0afb19cc07720f995b40b74521ca4a005d
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-11-13 11:33:40 -0600

    ch5 project 4 done

--- JavaScript/data-files/Chapter05/js05/project04/footnotes.js
+++ JavaScript/data-files/Chapter05/js05/project04/footnotes.js
@@ -4,8 +4,8 @@
       Project 05-04
 
       Project to display footnotes in a popup window
-      Author: 
-      Date:   
+      Author: Daniel Cheeley
+      Date:   11/13/25
 
       Filename: footnotes.js
 */
