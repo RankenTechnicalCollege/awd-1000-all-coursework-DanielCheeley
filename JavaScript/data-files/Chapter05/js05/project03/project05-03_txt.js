3d8d1dfa3294e8516047389fe5f119acf15ab603
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-11-13 10:43:41 -0600

    ch5 project3 finish

--- JavaScript/data-files/Chapter05/js05/project03/project05-03_txt.js
+++ JavaScript/data-files/Chapter05/js05/project03/project05-03_txt.js
@@ -4,9 +4,36 @@
       Project 05-03
 
       Project to create a table of headings from an article
-      Author: 
-      Date:   
+      Author: Daniel Cheeley
+      Date:   11/13/25
 
       Filename: project05-03.js
 */
 
+let sourceDoc = document.getElementById("source_doc");
+let toc = document.getElementById("toc");
+let headingCount = 1
+const HEADING = "H2";
+
+window.addEventListener('load', populate);
+
+function populate() {
+      for (let n = sourceDoc.firstElementChild; n != null; n = n.nextElementSibling) {
+            if (n.nodeName == HEADING) {
+                  let anchor = document.createElement('a');
+                  anchor.id = "doclink" + headingCount;
+                  n.insertBefore(anchor, n.firstChild);
+
+                  let listItem = document.createElement('li');
+                  let link = document.createElement('a');
+                  listItem.appendChild(link);
+
+                  link.textContent = n.textContent;
+                  link.href = "#doclink" + headingCount;
+
+                  toc.appendChild(listItem);
+
+                  headingCount++;
+            }
+      }
+}
