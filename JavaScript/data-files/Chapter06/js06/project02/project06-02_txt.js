5486f186e0edc6f4db83a309f5aeba8d1e2715c6
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-11-17 11:10:23 -0600

    ch6 project 2 finish

--- JavaScript/data-files/Chapter06/js06/project02/project06-02_txt.js
+++ JavaScript/data-files/Chapter06/js06/project02/project06-02_txt.js
@@ -4,10 +4,20 @@
       Project 06-02
 
       Project to turn a selection list into a selection of hypertext links
-      Author: 
-      Date:   
+      Author: Daniel Cheeley
+      Date:   11/17/25
 
       Filename: project06-02.js
 */
 
 
+window.addEventListener("load", function() {
+      let allSelect = this.document.querySelectorAll("form#govLinks select");
+
+      for(let i = 0; i < allSelect.length; i++) {
+            allSelect[i].addEventListener("change", function(evt) {
+                  let linkURL = evt.target.value;
+                  window.open(linkURL);
+            });
+      }
+});
