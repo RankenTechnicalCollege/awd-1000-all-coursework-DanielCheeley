d56b0b1ddce80c67883a79cca65f8f6c78078000
DanielCheeley <Daniel_Cheeley@insideranken.org>
2025-11-17 10:53:33 -0600

    ch6 project 1 finish

--- JavaScript/data-files/Chapter06/js06/project01/project06-01_txt.js
+++ JavaScript/data-files/Chapter06/js06/project01/project06-01_txt.js
@@ -4,9 +4,20 @@
       Project 06-01
 
       Project to validate a form used for setting up a new account
-      Author: 
-      Date:   
+      Author: Daniel Cheeley
+      Date:   11/17/25
 
       Filename: project06-01.js
 */
 
+submitButton.addEventListener("click", () => {
+    if (pwd.validity.patternMismatch) {
+        pwd.setCustomValidity("Your password must be at least 8 characters with at least one letter and one number");
+    }
+    else if (pwd.value !== pwd2.value) {
+        pwd.setCustomValidity("Your passwords must match");
+    }
+    else {
+        pwd.setCustomValidity("");
+    }
+});
